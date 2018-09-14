e886f75845f12b37e4b774d00d9c7c667642678f
Manuel Porto <manu.porto94@gmail.com>
2018-09-14 09:45:36 -0300

    Remove http package as it can't be used

--- cmd/dbserver/main.go
+++ cmd/dbserver/main.go
@@ -1,25 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/manuporto/distributedHTTPServer/pkg/filemanager"
-	"log"
-	"net/http"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		title := r.URL.Path[1:] + ".json"
-		b, _ := filemanager.LoadFile(title)
-		fmt.Fprintf(w, "<h1>File</h1><div>%s</div>", b)
-	case http.MethodDelete:
-		filemanager.DeleteFile(r.URL.Path[1:])
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			c.Write([]byte("bye bye"))
+			break
+		}
+
+		result := strconv.Itoa(rand.Int()) + "\n"
+		c.Write([]byte(string(result)))
 	}
+	c.Close()
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+	rand.Seed(time.Now().Unix())
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
 }
