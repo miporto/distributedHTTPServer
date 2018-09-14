b898f3dd6733459f29a12d853f50cad30b189c9b
Manuel Porto <manu.porto94@gmail.com>
2018-09-14 12:56:05 -0300

    Fix db address

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -4,17 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "db:8080")
+	conn, err := net.Dial("tcp", "distributedhttpserver_db_1:8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 	fmt.Println("Connection established, sending packets")
-	fmt.Fprintf(conn, "Hello\n")
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(conn, "hello\n")
 	status, err := bufio.NewReader(conn).ReadString('\n')
 	fmt.Println(status)
 	fmt.Fprintf(conn, "STOP\n")
