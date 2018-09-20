e42913b6e7e1dd97cdce582837742b40f7c2f1c4
Manuel Porto <manu.porto94@gmail.com>
2018-09-20 01:55:16 -0300

    Fix reading of whole http frame

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -11,13 +11,11 @@ import (
 	"github.com/manuporto/distributedHTTPServer/pkg/server"
 )
 
-func readHeader(c net.Conn) (string, error) {
-	r := bufio.NewReader(c)
+func readHeader(r *bufio.Reader) (string, error) {
 	b, err := r.ReadString('\n')
 	var header strings.Builder
 	header.WriteString(b)
 	for {
-		fmt.Print(b)
 		if err != nil {
 			return "", err
 		}
@@ -30,29 +28,27 @@ func readHeader(c net.Conn) (string, error) {
 	}
 }
 
-func readBody(c net.Conn, hh httpparser.HttpHeader) ([]byte, error) {
-	fmt.Println("Making body of ", hh.ContentLength)
+func readBody(r *bufio.Reader, hh httpparser.HttpHeader) ([]byte, error) {
 	body := make([]byte, hh.ContentLength)
-	_, err := bufio.NewReader(c).Read(body)
+	// TODO handle read < body
+	_, err := r.Read(body)
 	return body, err
 }
 
 func readRequest(c net.Conn) (*httpparser.HttpFrame, error) {
-	header, err := readHeader(c)
+	r := bufio.NewReader(c)
+	header, err := readHeader(r)
 	fmt.Print(header)
 	if err != nil {
 		return nil, err
 	}
 	httpheader := httpparser.GetHeader(header)
-	fmt.Println("reading body...")
-	body, err := readBody(c, httpheader)
-	fmt.Println("Body read!")
-	return &httpparser.HttpFrame{httpheader, body}, err
+	body, err := readBody(r, httpheader)
+	return &httpparser.HttpFrame{Header: httpheader, Body: body}, err
 }
 
 func handleGET(hf *httpparser.HttpFrame) []byte {
-	fmt.Println(hf.Header.Uri)
-	f, err := filemanager.LoadFile(hf.Header.Uri[1:])
+	f, err := filemanager.LoadFile(hf.Header.Uri)
 	if err != nil {
 		fmt.Println("Invalid file: ", err)
 		return nil
@@ -61,42 +57,44 @@ func handleGET(hf *httpparser.HttpFrame) []byte {
 }
 
 func handlePOST(hf *httpparser.HttpFrame) error {
-	fmt.Println("Handling POST...")
-	return filemanager.SaveFile(hf.Header.Uri[1:], hf.Body)
+	return filemanager.SaveFile(hf.Header.Uri, hf.Body)
 }
 
-func handlePUT() {
-
+func handlePUT(hf *httpparser.HttpFrame) {
+	filemanager.UpdateFile(hf.Header.Uri, hf.Body)
 }
 
-func handleDELETE() {
-
+func handleDELETE(hf *httpparser.HttpFrame) {
+	filemanager.DeleteFile(hf.Header.Uri)
 }
 
 func handleConnection(c net.Conn) {
+	defer c.Close()
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-	for {
-		req, err := readRequest(c)
-		if err != nil {
-			// TODO handle error
-			c.Write([]byte(err.Error()))
-		}
-		fmt.Println(req.Header.Method)
-		switch req.Header.Method {
-		case httpparser.MethodGet:
-			f := handleGET(req)
-			fmt.Printf(string(f))
-			c.Write(f)
-		case httpparser.MethodPost:
-			err = handlePOST(req)
-			c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-		}
+	req, err := readRequest(c)
+	if err != nil {
+		// TODO handle error
+		fmt.Println("Closing: ", err)
+		c.Write([]byte(err.Error()))
+		return
 	}
-	c.Close()
+	req.Header.Uri = req.Header.Uri[1:] + ".json"
+	switch req.Header.Method {
+	case httpparser.MethodGet:
+		f := handleGET(req)
+		fmt.Printf(string(f))
+	case httpparser.MethodPost:
+		handlePOST(req)
+	case httpparser.MethodPut:
+		handlePUT(req)
+	case httpparser.MethodDelete:
+		handleDELETE(req)
+
+	}
+	c.Write([]byte("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"))
 }
 
 func main() {
-	sv := server.Server{":8080", handleConnection}
+	sv := server.Server{Address: ":8080", Handler: handleConnection}
 	sv.Serve()
 }
