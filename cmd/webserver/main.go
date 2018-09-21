1d15f7aa664e3af2b6fa7fee9667ba3b2f6b6292
Manuel Porto <manu.porto94@gmail.com>
2018-09-21 15:55:21 -0300

    Stop handling http requests directly since that it's db server's job

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"strings"
 
-	"github.com/manuporto/distributedHTTPServer/pkg/filemanager"
 	"github.com/manuporto/distributedHTTPServer/pkg/httpparser"
 	"github.com/manuporto/distributedHTTPServer/pkg/server"
 )
@@ -30,7 +29,6 @@ func readHeader(r *bufio.Reader) (string, error) {
 
 func readBody(r *bufio.Reader, size int) ([]byte, error) {
 	body := make([]byte, size)
-	// TODO handle read < body
 	_, err := r.Read(body)
 	return body, err
 }
@@ -47,27 +45,6 @@ func readRequest(c net.Conn) (*httpparser.HttpFrame, error) {
 	return &httpparser.HttpFrame{Header: httpheader, Body: body}, err
 }
 
-func handleGET(hf *httpparser.HttpFrame) []byte {
-	f, err := filemanager.LoadFile(hf.Header.Uri)
-	if err != nil {
-		fmt.Println("Invalid file: ", err)
-		return nil
-	}
-	return f
-}
-
-func handlePOST(hf *httpparser.HttpFrame) error {
-	return filemanager.SaveFile(hf.Header.Uri, hf.Body)
-}
-
-func handlePUT(hf *httpparser.HttpFrame) {
-	filemanager.UpdateFile(hf.Header.Uri, hf.Body)
-}
-
-func handleDELETE(hf *httpparser.HttpFrame) {
-	filemanager.DeleteFile(hf.Header.Uri)
-}
-
 func handleConnection(c net.Conn) {
 	defer c.Close()
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
@@ -79,22 +56,10 @@ func handleConnection(c net.Conn) {
 		return
 	}
 	req.Header.Uri = req.Header.Uri[1:] + ".json"
-	switch req.Header.Method {
-	case httpparser.MethodGet:
-		f := handleGET(req)
-		fmt.Printf(string(f))
-	case httpparser.MethodPost:
-		handlePOST(req)
-	case httpparser.MethodPut:
-		handlePUT(req)
-	case httpparser.MethodDelete:
-		handleDELETE(req)
-
-	}
 	c.Write([]byte("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"))
 }
 
 func main() {
-	sv := server.Server{Address: ":8080", Handler: handleConnection}
+	sv := server.NewServer(":8080", handleConnection)
 	sv.Serve()
 }
