15447b4ae93bd526cdd2bde32f5db0916b5dd85f
Manuel Porto <manu.porto94@gmail.com>
2018-09-23 14:22:57 -0300

    Receive config options and address through parameters

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net"
-
 	"github.com/manuporto/distributedHTTPServer/pkg/httpserver"
+	"github.com/manuporto/distributedHTTPServer/pkg/util"
+	"net"
+	"os"
+	"strconv"
 
 	"github.com/manuporto/distributedHTTPServer/pkg/httpparser"
 	"github.com/manuporto/distributedHTTPServer/pkg/server"
 )
 
-func fowardRequest(req *httpparser.HttpFrame) (*httpparser.HttpFrame, error) {
-	c, err := net.Dial("tcp4", ":8081")
+func calculateDestServer(uri string, dbServerName string, port string, numOfDbServers int) string {
+	uriHash := util.CalculateHash(uri)
+	dbnum := strconv.Itoa(int(uriHash)%numOfDbServers + 1)
+	return dbServerName + dbnum + port
+}
+
+func fowardRequest(req *httpparser.HttpFrame, address string) (*httpparser.HttpFrame, error) {
+	c, err := net.Dial("tcp4", address)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
@@ -31,11 +39,24 @@ func handleConnection(c net.Conn) {
 		c.Write([]byte(err.Error()))
 		return
 	}
-	res, err := fowardRequest(req)
+
+	dbServerName := os.Getenv("DBSRVNAME")
+	numOfDbServers, _ := strconv.Atoi(os.Getenv("DBSRVS"))
+	destServer := calculateDestServer(
+		req.Header.URI,
+		dbServerName,
+		os.Getenv("DBSRVPORT"),
+		numOfDbServers)
+	fmt.Println("Destination: ", destServer)
+	res, err := fowardRequest(req, destServer)
 	c.Write([]byte(res.Raw))
 }
 
 func main() {
-	sv := server.NewServer(":8080", handleConnection)
+	if len(os.Args) != 2 {
+		fmt.Println("Wrong number of args\n Usage: ./dbserver <address>")
+		return
+	}
+	sv := server.NewServer(os.Args[1], handleConnection)
 	sv.Serve()
 }
