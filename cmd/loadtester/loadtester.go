0628d3f5ccec11335485457ace6c023840911d0b
Manuel Porto <manu.porto94@gmail.com>
2018-09-24 17:06:41 -0300

    Create simple load tester

---
+++ cmd/loadtester/loadtester.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	origin = "origin_"
+	entity = "entity_"
+)
+
+func generateRandomURI() string {
+	var uri strings.Builder
+	originNum := strconv.Itoa(rand.Intn(10))
+	entityNum := strconv.Itoa(rand.Intn(5))
+	id := strconv.Itoa(rand.Intn(10))
+	uri.WriteString("/")
+	uri.WriteString(origin)
+	uri.WriteString(originNum)
+	uri.WriteString("/")
+	uri.WriteString(entity)
+	uri.WriteString(entityNum)
+	uri.WriteString("/")
+	uri.WriteString(id)
+
+	return uri.String()
+}
+
+func getWorker(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	start := time.Now()
+	fullUrl := url + generateRandomURI()
+	resp, err := http.Get(fullUrl)
+	if err != nil {
+		log.Printf("ERROR: [GET %s] failed due to %s", fullUrl, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	secs := time.Since(start).Seconds()
+	log.Printf("INFO: [GET %s] Elapsed: %v response status: %s", fullUrl, secs, resp.Status)
+	//body, err := ioutil.ReadAll(resp.Body)
+	//if err != nil {
+	//	log.Printf("ERROR: [%s] failed due to %s", fullUrl, err.Error())
+	//	return
+	//}
+	//log.Println(string(body))
+}
+
+func postWorker(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	start := time.Now()
+	fullUrl := url + generateRandomURI()
+	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	req, err := http.NewRequest("POST", fullUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("ERROR: [POST %s] failed due to %s", fullUrl, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	secs := time.Since(start).Seconds()
+	log.Printf("INFO: [POST %s] Elapsed: %v response status: %s", fullUrl, secs, resp.Status)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Wrong number of args\n Usage: ./loadtester <address>")
+		return
+	}
+
+	var wg sync.WaitGroup
+	workers := 4000
+	wg.Add(workers)
+	for i := 0; i < (workers / 2); i++ {
+		go postWorker(os.Args[1], &wg)
+		go getWorker(os.Args[1], &wg)
+	}
+	wg.Wait()
+}
