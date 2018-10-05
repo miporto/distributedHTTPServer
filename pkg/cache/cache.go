d9895f43ad7137fe0d77bb5d9e53f0b72d26bad5
Manuel Porto <manu.porto94@gmail.com>
2018-10-04 21:38:08 -0300

    Start refactoring cache

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -13,20 +13,38 @@ type cacheEntry struct {
 }
 
 type Cache struct {
+	l     *sync.RWMutex
 	slots []*cacheEntry
 	size  uint
 }
 
 func NewCache(size uint) Cache {
 	slots := make([]*cacheEntry, size)
+	var l sync.RWMutex
 	if size == 0 {
-		return Cache{slots, size}
+		return Cache{&l, slots, size}
 	}
 	for i := uint(0); i < size; i++ {
 		var ce cacheEntry
 		slots[i] = &ce
 	}
-	return Cache{slots, size}
+	return Cache{&l, slots, size}
+}
+
+func (c *Cache) Get2(path string) ([]byte, bool) {
+	if c.size == 0 {
+		return nil, false
+	}
+	c.l.RLock()
+	defer c.l.RUnlock()
+	for _, element := range c.slots {
+		if element.path == "" {
+			break
+		} else if element.path == path {
+			return element.body, true
+		}
+	}
+	return nil, false
 }
 
 func (c Cache) Get(path string) ([]byte, bool) {
@@ -43,6 +61,35 @@ func (c Cache) Get(path string) ([]byte, bool) {
 	return slot.body, true
 }
 
+func (c *Cache) Insert(path string, body []byte) {
+	if c.size == 0 {
+		return
+	}
+	c.l.Lock()
+	defer c.l.Unlock()
+	for _, element := range c.slots {
+		if element.path == "" {
+			element.path = path
+			element.body = body
+			return
+		}
+	}
+}
+
+func (c *Cache) Update2(path string, body []byte) {
+	if c.size == 0 {
+		return
+	}
+	c.l.Lock()
+	defer c.l.Unlock()
+	for _, element := range c.slots {
+		if element.path == path {
+			element.body = body
+			return
+		}
+	}
+}
+
 func (c Cache) Update(path string, body []byte) {
 	if c.size == 0 {
 		return
@@ -53,6 +100,21 @@ func (c Cache) Update(path string, body []byte) {
 	defer slot.Unlock()
 	slot.path = path
 	slot.body = body
+}
+
+func (c *Cache) Delete2(path string) {
+	if c.size == 0 {
+		return
+	}
+	c.l.Lock()
+	defer c.l.Unlock()
+	for _, element := range c.slots {
+		if element.path == path {
+			element.path = ""
+			element.body = nil
+			return
+		}
+	}
 }
 
 func (c Cache) Delete(path string) {
